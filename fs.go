fee8f62f24426f80b21cf7d74a34a53dec74bb93
gnue <gnue@so-kukan.com>
2015-05-20 16:22:24 +0900

    ディレクトリがひとつのみのときは合成せずに元のディレクトリを返す

--- fs.go
+++ fs.go
@@ -14,42 +14,63 @@ func New(fileSystems ...http.FileSystem) *UnionFS {
 }
 
 func (u *UnionFS) Open(name string) (http.File, error) {
-	var dir *Dir
+	var dirFSs []http.FileSystem
 
 	for _, fs := range u.fileSystems {
 		file, err := fs.Open(name)
-		if err == nil {
-			fi, err := file.Stat()
-			if err != nil {
-				file.Close()
-				return nil, err
-			}
-
-			if fi.IsDir() {
-				if dir == nil {
-					dir = &Dir{fi: &FileInfo{name: fi.Name(), modTime: fi.ModTime()}}
-				}
-
-				files, err := file.Readdir(-1)
-				file.Close()
-				if err != nil {
-					continue
-				}
-
-				dir.addFile(files...)
-			} else {
-				if dir != nil {
-					file.Close()
-					continue
-				}
-
-				return file, nil
-			}
+		if err != nil {
+			continue
 		}
+
+		fi, err := file.Stat()
+		if err != nil {
+			file.Close()
+			return nil, err
+		}
+
+		if !fi.IsDir() && dirFSs == nil {
+			return file, nil
+		}
+
+		file.Close()
+		dirFSs = append(dirFSs, fs)
 	}
 
-	if dir != nil {
-		return dir, nil
+	if len(dirFSs) == 1 {
+		return dirFSs[0].Open(name)
+	}
+
+	if dirFSs != nil {
+		var dir *Dir
+
+		for _, fs := range dirFSs {
+			file, err := fs.Open(name)
+			if err != nil {
+				continue
+			}
+
+			var files []os.FileInfo
+
+			fi, err := file.Stat()
+			if err == nil {
+				files, err = file.Readdir(-1)
+			}
+			file.Close()
+
+			if err != nil {
+				continue
+			}
+
+			if dir == nil {
+				dir = &Dir{fi: &FileInfo{name: fi.Name(), modTime: fi.ModTime()}}
+			}
+
+			dir.addFile(files...)
+		}
+
+		if dir != nil {
+			return dir, nil
+		}
 	}
 
 	return nil, os.ErrNotExist
