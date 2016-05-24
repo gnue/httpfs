2d8721ce420575f49b70348d0a780eb0b016e49c
gnue <gnue@so-kukan.com>
2016-05-24 22:56:15 +0900

    Example にコメントを追加

--- example_test.go
+++ example_test.go
@@ -6,6 +6,20 @@ import (
 	"github.com/gnue/httpfs/indexfs"
 )
 
+/*
+Simple index webserver
+
+```go
+indexes := indexfs.Indexes{"index.html", "index.htm"}
+fs := indexfs.New(http.Dir("test"), indexes.DirIndex)
+```
+
+```go
+indexes := indexfs.Indexes{"index.html", "index.htm"}
+fs := indexfs.New(http.Dir("test"), indexes.AutoIndex)
+```
+
+*/
 func ExampleIndexFS() {
 	fs := indexfs.New(http.Dir("sites"), func(fs http.FileSystem, dir string) (http.File, error) {
 		return indexfs.OpenIndex(fs, dir, "index.html", "index.htm")
