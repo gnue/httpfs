d00e9f275c43f378420d1b86586179b1eed622d3
gnue <gnue@so-kukan.com>
2018-02-26 11:59:31 +0900

    ExampleNew を追加

--- zipfs/example_test.go
+++ zipfs/example_test.go
@@ -1,13 +1,30 @@
 package zipfs_test
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gnue/httpfs/zipfs"
 )
 
-// Simple zip webserver
+// Simple zip webserver(use New)
+func ExampleNew() {
+	b, err := ioutil.ReadFile("public.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs, err := zipfs.New(b, &zipfs.Options{Prefix: "public"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", http.FileServer(fs))
+	http.ListenAndServe(":8080", nil)
+}
+
+// Simple zip webserver(use Open)
 func ExampleOpen() {
 	fs, err := zipfs.Open("public.zip", &zipfs.Options{Prefix: "public"})
 	if err != nil {
